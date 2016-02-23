53f76e235471cd83bb214a76619d77fbba4084f7
jonli <jonli@tencent.com>
2016-02-23 16:01:03 +0800

    update selected

--- js/handler.js
+++ js/handler.js
@@ -5,11 +5,11 @@ function gotoTop(min_height){
     //将返回顶部的html代码插入页面上id为page的元素的末尾 
     // $("#page").append(gotoTop_html);
     $("#gotoTop").click(//定义返回顶部点击向上滚动的动画
-        function(){$('html,body').animate({scrollTop:0},700);
+      function(){$('html,body').animate({scrollTop:0},700);
     }).hover(//为返回顶部增加鼠标进入的反馈效果，用添加删除css类实现
-        function(){$(this).addClass("hover");},
-        function(){$(this).removeClass("hover");
-    });
+    function(){$(this).addClass("hover");},
+    function(){$(this).removeClass("hover");
+  });
     //获取页面的最小高度，无传入值则默认为600像素
     min_height ? min_height = min_height : min_height = 600;
     //为窗口的scroll事件绑定处理函数
@@ -18,19 +18,39 @@ function gotoTop(min_height){
         var s = $(window).scrollTop();
         //当窗口的滚动条的垂直位置大于页面的最小高度时，让返回顶部元素渐现，否则渐隐
         if( s > min_height){
-            $("#gotoTop").fadeIn(100);
+          $("#gotoTop").fadeIn(100);
         }else{
-            $("#gotoTop").fadeOut(200);
+          $("#gotoTop").fadeOut(200);
         };
-    });
+      });
 };
 
 function small_clicked(evt) {
  // alert("clicked");
  var img_src = evt.target.getAttribute('src');
- $("#large_img").attr("src", img_src); 
+ if (!img_src) {
+  var imgs = evt.target.getElementsByTagName('img');
+  img_src = imgs[0].getAttribute('src');
+ }
+ if (img_src) {
+   $("#large_img").attr("src", img_src); 
+ }
 }
 
+window.onload = function() {
+  gotoTop();
 
-window.onload = function(){gotoTop();}
-('src');
+    // var firstItem = $('#scroller li:first-child a');
+    // if (firstItem) {
+    //   firstItem[0].trigger("click");
+    // }
+    var firstItem = $('#scroller li').first().find("a");
+    if (firstItem) {
+      // firstItem.trigger("click");
+      var imgs = firstItem[0].getElementsByTagName('img');
+      var img_src = imgs[0].getAttribute('src');
+      if (img_src) {
+       $("#large_img").attr("src", img_src); 
+     }
+   }
+}
